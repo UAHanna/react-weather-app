23cf5cfab96cbe854a3ea4b02174a579a718c379
UAHanna <119074624+UAHanna@users.noreply.github.com>
2023-02-22 19:43:14 +0200

    add Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was created by Hanna and is{" "}
+        <a href="https://github.com/UAHanna/react-weather-app" target="_blank">
+          open-soursed on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
